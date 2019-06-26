d6d2fd50cde906da8351e9d91dbda63cd9af93bc
ksolvesnutra <sujeet.kumar@ksolves.com>
2019-06-26 10:35:23 +0530

    added-lazy oding concept

--- src/root.js
+++ src/root.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React,{Suspense,lazy} from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import {Button, ButtonGroup} from 'react-bootstrap';
-import BookRecord from './bookRecord';
-import Add from './add';
+const AsyncBook = lazy(() =>import('./bookRecord'));
+const AsyncAdd = lazy(() =>import('./add'));
+// import BookRecord from './bookRecord';
+// import Add from './add';
 
 function Root() {
   return (
     <div className="container">
-    <Router>
-      <div className="d-flex flex-column">
-        <ButtonGroup size="sm">
-          <Button variant="outline-warning"><Link to="/records">Show Records</Link></Button>
-          <Button variant="outline-success"><Link to="/add">Add Records</Link></Button>
-        </ButtonGroup>
-      </div>
-        <Route path="/records" component={BookRecord} />
-        <Route path="/add" component={Add} />
-      </Router>
+    <Suspense fallback={<div> loading... </div>}>
+      <Router>
+        <div className="d-flex flex-column">
+          <ButtonGroup size="sm">
+            <Button variant="outline-warning"><Link to="/records">Show Records</Link></Button>
+            <Button variant="outline-success"><Link to="/add">Add Records</Link></Button>
+          </ButtonGroup>
+        </div>
+          <Route path="/records" component={AsyncBook} />
+          <Route path="/add" component={AsyncAdd} />
+        </Router>
+      </Suspense>
     </div>
   );
 }
